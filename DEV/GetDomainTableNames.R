ffe811c3641d90acc67ffdf0a307f456fa8c872c
Johan van Tent <tentvanjohan@hotmail.com>
2021-03-04 17:18:12 +0100

    querytekst ingekort en domeintabelsoort toegevoegd

--- DEV/GetDomainTableNames.R
+++ DEV/GetDomainTableNames.R
@@ -2,10 +2,7 @@ GetDomainTableNames <- function(pCheckDate = toString(Sys.Date())) {
   message(paste("pCheckDate", pCheckDate))
 
   tekstUrl <- "https://www.aquo.nl/index.php"
-  opmaakJson <- paste0("%2F&format=json&link=none&headers=show&searchlabel=JSON&class=sortable+wikitable+smwtable",
-    "&theme=bootstrap&offset=0&limit=500",
-    "&mainlabel=&prettyprint=true&unescape=true"
-  )
+  opmaakJson <- paste0("%2F&format=json", "&limit=500")
 
   categorie <- "?title=Speciaal:Vragen&x=-5B-5BElementtype%3A%3ADomeintabel%20%7C%7C%20Domeintabeltechnisch%20%7C%7C%20Domeintabelverzamellijst-5D-5D-20"
   kenmerken <- "%2F-3FElementtype%2F-3FVoorkeurslabel%2F-3FMetadata"
@@ -16,18 +13,19 @@ GetDomainTableNames <- function(pCheckDate = toString(Sys.Date())) {
   if (req$status_code != 200 || length(req$content) == 0) {
     message("Geen domeintabellen gevonden")
     return(NULL)
-
   }
 
   domeintabellen_json <- jsonlite::fromJSON(httr::content(req, "text", encoding = "UTF-8"))$results
 
   domeintabellen <- tibble::tibble(domeintabel = purrr::map_chr(domeintabellen_json, list("printouts", "Voorkeurslabel")),
-                                   guid        = purrr::map_chr(domeintabellen_json, "fulltext"),
-                                   kolommen    = purrr::map(domeintabellen_json, list("printouts", "Metadata")))
+                                   domeintabelsoort = purrr::map_chr(domeintabellen_json, list("printouts", "Elementtype", "fulltext")),
+                                   kolommen    = purrr::map(domeintabellen_json, list("printouts", "Metadata")),
+                                   guid        = purrr::map_chr(domeintabellen_json, "fulltext"))
 
   return(domeintabellen)
 }
 
+View(GetDomainTableNames() )
 GetDomainTableNames()$kolommen[[1]]
 
 nrow(GetDomainTableNames()) == 132
