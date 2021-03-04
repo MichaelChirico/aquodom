186b6774afc48f776e13ea398e3f19dff79de007
Johan van Tent <tentvanjohan@hotmail.com>
2021-03-04 16:50:05 +0100

    Overbodige code verwijderd en ingekort

--- DEV/GetDomainTableNames.R
+++ DEV/GetDomainTableNames.R
@@ -1,50 +1,34 @@
 GetDomainTableNames <- function(pCheckDate = toString(Sys.Date())) {
   message(paste("pCheckDate", pCheckDate))
-  library(jsonlite)
-  library(httr)
-  library(RCurl)
-
-  maakTekstURL <- function(tekstURL, categorie, beperking, kenmerken, opmaak) {
-    returnstring <- paste(tekstURL, categorie, beperking, kenmerken, opmaak, sep = "")
-    return(returnstring)
-  }
-
-  bewerkDatum <- function(pDatum) {
-    lDatum <- substring(toString(pDatum), 3, nchar(pDatum))
-    lDatum <- str_replace(lDatum, "/0/0/0/0", "")
-    lDatum <- toString(lubridate::parse_date_time(lDatum, orders = "ymd"))
-    return(lDatum)
-  }
-
-  domeinTabellen <- data.frame(matrix(ncol = 1, nrow = 0))
-  colnames(domeinTabellen) <- as.list("Domeintabel")
 
   tekstUrl <- "https://www.aquo.nl/index.php"
-  lStartPage <- 0
-  lLimit <- 1
-  curl <- getCurlHandle()
-  opmaakJson <- paste("%2F&format=json&link=none&headers=show&searchlabel=JSON&class=sortable+wikitable+smwtable",
+  opmaakJson <- paste0("%2F&format=json&link=none&headers=show&searchlabel=JSON&class=sortable+wikitable+smwtable",
     "&theme=bootstrap&offset=0&limit=500",
-    "&mainlabel=&prettyprint=true&unescape=true",
-    sep = ""
+    "&mainlabel=&prettyprint=true&unescape=true"
   )
 
   categorie <- "?title=Speciaal:Vragen&x=-5B-5BElementtype%3A%3ADomeintabel%20%7C%7C%20Domeintabeltechnisch%20%7C%7C%20Domeintabelverzamellijst-5D-5D-20"
   kenmerken <- "%2F-3FElementtype%2F-3FVoorkeurslabel%2F-3FMetadata"
-  beperking <- paste("-5B-5BBegin-20geldigheid::<=", pCheckDate, "-5D-5D-5B-5BEind-20geldigheid::>=", pCheckDate, "-5D-5D", sep = "")
-  json_file <- maakTekstURL(tekstUrl, categorie, beperking, kenmerken, opmaakJson)
-  # message(json_file)
-  req <- httr::GET(json_file, curl = curl)
+  beperking <- paste0("-5B-5BBegin-20geldigheid::<=", pCheckDate, "-5D-5D-5B-5BEind-20geldigheid::>=", pCheckDate, "-5D-5D")
+  json_file <- paste0(tekstUrl, categorie, beperking, kenmerken, opmaakJson)
+  req <- httr::GET(json_file)
 
-  if (req$status_code == 200 && length(req$content) > 0) {
-    domeinTabellenJson <- jsonlite::fromJSON(httr::content(req, "text", encoding = "UTF-8"))$results
+  if (req$status_code != 200 || length(req$content) == 0) {
+    message("Geen domeintabellen gevonden")
+    return(NULL)
 
-    for (di in 1:length(domeinTabellenJson)) {
-      domeinTabellen[di, "Domeintabel"] <- domeinTabellenJson[[di]]$printouts$Voorkeurslabel
-    }
   }
-  else {
-    message("Geen domeintabellen aanwezig")
-  }
-  return(domeinTabellen)
+
+  domeintabellen_json <- jsonlite::fromJSON(httr::content(req, "text", encoding = "UTF-8"))$results
+
+  domeintabellen <- tibble::tibble(domeintabel = purrr::map_chr(domeintabellen_json, list("printouts", "Voorkeurslabel")),
+                                   guid        = purrr::map_chr(domeintabellen_json, "fulltext"),
+                                   kolommen    = purrr::map(domeintabellen_json, list("printouts", "Metadata")))
+
+  return(domeintabellen)
 }
+
+GetDomainTableNames()$kolommen[[1]]
+
+nrow(GetDomainTableNames()) == 132
+nrow(GetDomainTableNames("2020-01-01")) == 127
