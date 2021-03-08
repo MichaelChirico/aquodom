ee2ef9b3963a66ff83a22d37930fb445f167a645
Johan van Tent <tentvanjohan@hotmail.com>
2021-03-08 14:25:06 +0100

    wijzigingsdatum toegevoegd

--- R/domeintabel_namen.R
+++ R/domeintabel_namen.R
@@ -1,10 +1,15 @@
 domeintabel_namen_basis <- function(peildatum) {
 
+  verwerk_datum <- function(datum_raw){
+    stringr::str_extract(datum_raw, "\\d{4}/\\d{1,}/\\d{1,}/\\d{1,}/\\d{1,}/\\d{1,}") %>%
+    lubridate::ymd_hms(tz = "CET")
+  }
+
   # Query samenstellen
   tekstUrl <- "https://www.aquo.nl/index.php"
   opmaakJson <- paste0("%2F&format=json", "&limit=500")
   categorie <- "?title=Speciaal:Vragen&x=-5B-5BElementtype%3A%3ADomeintabel%20%7C%7C%20Domeintabeltechnisch%20%7C%7C%20Domeintabelverzamellijst-5D-5D-20"
-  kenmerken <- "%2F-3FElementtype%2F-3FVoorkeurslabel%2F-3FMetadata"
+  kenmerken <- "%2F-3FElementtype%2F-3FVoorkeurslabel%2F-3FMetadata%2F-3FWijzigingsdatum"
   beperking <- paste0("-5B-5BBegin-20geldigheid::<=", peildatum, "-5D-5D-5B-5BEind-20geldigheid::>=", peildatum, "-5D-5D")
 
   json_file <- paste0(tekstUrl, categorie, beperking, kenmerken, opmaakJson)
@@ -19,8 +24,10 @@ domeintabel_namen_basis <- function(peildatum) {
 
   domeintabellen <- tibble::tibble(domeintabel = purrr::map_chr(domeintabellen_json, list("printouts", "Voorkeurslabel")),
                                    domeintabelsoort = purrr::map_chr(domeintabellen_json, list("printouts", "Elementtype", "fulltext")),
+                                   datum_wijziging = purrr::map_chr(domeintabellen_json, list("printouts", "Wijzigingsdatum", "raw")),
                                    kolommen    = purrr::map(domeintabellen_json, list("printouts", "Metadata")),
-                                   guid        = purrr::map_chr(domeintabellen_json, "fulltext"))
+                                   guid        = purrr::map_chr(domeintabellen_json, "fulltext")) %>%
+    dplyr::mutate(datum_wijziging = verwerk_datum(datum_wijziging))
 
   return(domeintabellen)
 }
@@ -52,3 +59,4 @@ domeintabel_guid <- function(namen, peildatum = NULL){
     unname()
 
 }
+
--- tests/testthat/test-domeintabel_namen.R
+++ tests/testthat/test-domeintabel_namen.R
@@ -1,17 +1,20 @@
 # Alleen lokaaltesten ivm tijd
 
 test_that("domeintabel_namen_basis", {
-  skip("Duurt relatief lang")
+  # skip("Duurt relatief lang")
+  skip_on_cran()
+
   expect_equal(nrow(domeintabel_namen_basis("2020-01-01")), 127)
 })
 
 test_that("Caching domeintabel_namen", {
-  skip("Duurt relatief lang")
-
+  # skip("Duurt relatief lang")
+  skip_on_cran()
   domeintabel_namen()
   expect_true(system.time(domeintabel_namen())[3] < 0.5)
 })
 
 test_that("is_domeintabel",{
+  skip_on_cran()
   expect_equal(is_domeintabel(c("Hoedanigheid", "Domeintabel")), c(TRUE, FALSE))
 })
