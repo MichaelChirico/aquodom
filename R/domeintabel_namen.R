882469eb13efb1af4bb434a27d08d1b076ca1431
Johan van Tent <tentvanjohan@hotmail.com>
2021-03-30 09:47:44 +0200

    peildatum  verwijderd als argument

--- R/domeintabel_namen.R
+++ R/domeintabel_namen.R
@@ -1,18 +1,21 @@
-domeintabel_namen_basis <- function(peildatum) {
+conv_timestamp <- function(timestamp){
+  timestamp %>%
+    as.numeric() %>%
+    as.POSIXct(origin = as.POSIXct("1970-01-01 00:00:00"), tz = "CET") %>%
+    lubridate::as_date()
+}
 
-  verwerk_datum <- function(datum_raw){
-    stringr::str_extract(datum_raw, "\\d{4}/\\d{1,}/\\d{1,}/\\d{1,}/\\d{1,}/\\d{1,}") %>%
-    lubridate::ymd_hms(tz = "CET")
-  }
+
+domeintabel_namen_basis <- function() {
+
 
   # Query samenstellen
   tekstUrl <- "https://www.aquo.nl/index.php"
-  opmaakJson <- paste0("%2F&format=json", "&limit=500")
-  categorie <- "?title=Speciaal:Vragen&x=-5B-5BElementtype%3A%3ADomeintabel%20%7C%7C%20Domeintabeltechnisch%20%7C%7C%20Domeintabelverzamellijst-5D-5D-20"
-  kenmerken <- "%2F-3FElementtype%2F-3FVoorkeurslabel%2F-3FMetadata%2F-3FWijzigingsdatum"
-  beperking <- paste0("-5B-5BBegin-20geldigheid::<=", peildatum, "-5D-5D-5B-5BEind-20geldigheid::>=", peildatum, "-5D-5D")
+  opmaakJson <- paste0("%2F&format=json")
+  categorie <- "?title=Speciaal:Vragen&x=%5B%5BElementtype%3A%3ADomeintabel%20%7C%7C%20Domeintabeltechnisch%20%7C%7C%20Domeintabelverzamellijst%5D%5D%20"
+  kenmerken <- "%2F%3FElementtype%2F%3FVoorkeurslabel%2F%3FMetadata%2F%3FWijzigingsdatum%2F%3FBegin%20geldigheid%2F%3FEind%20geldigheid"
 
-  json_file <- paste0(tekstUrl, categorie, beperking, kenmerken, opmaakJson)
+  json_file <- paste0(tekstUrl, categorie, kenmerken, opmaakJson)
   req <- httr::GET(json_file)
 
   if (req$status_code != 200 || length(req$content) == 0) {
@@ -24,10 +27,14 @@ domeintabel_namen_basis <- function(peildatum) {
 
   domeintabellen <- tibble::tibble(domeintabel = purrr::map_chr(domeintabellen_json, list("printouts", "Voorkeurslabel")),
                                    domeintabelsoort = purrr::map_chr(domeintabellen_json, list("printouts", "Elementtype", "fulltext")),
-                                   datum_wijziging = purrr::map_chr(domeintabellen_json, list("printouts", "Wijzigingsdatum", "raw")),
+                                   datum_wijziging = purrr::map_chr(domeintabellen_json, list("printouts", "Wijzigingsdatum", "timestamp")),
+                                   begin_geldigheid = purrr::map_chr(domeintabellen_json, list("printouts", "Begin geldigheid", "timestamp")),
+                                   eind_geldigheid = purrr::map_chr(domeintabellen_json, list("printouts", "Eind geldigheid", "timestamp")),
                                    kolommen    = purrr::map(domeintabellen_json, list("printouts", "Metadata")),
                                    guid        = purrr::map_chr(domeintabellen_json, "fulltext")) %>%
-    dplyr::mutate(datum_wijziging = verwerk_datum(datum_wijziging))
+    dplyr::mutate(datum_wijziging  = conv_timestamp(datum_wijziging),
+                  begin_geldigheid = conv_timestamp(begin_geldigheid),
+                  eind_geldigheid  = conv_timestamp(eind_geldigheid))
 
   return(domeintabellen)
 }
@@ -40,7 +47,7 @@ domeintabel_namen <- function(peildatum = NULL){
 
   if (is.null(peildatum)) peildatum <- toString(Sys.Date())
 
-    domeintabel_namen_m(peildatum)
+    domeintabel_namen_m()
 }
 
 is_domeintabel <- function(namen, peildatum = NULL){
