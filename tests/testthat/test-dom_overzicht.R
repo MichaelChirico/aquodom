6c8c6b325dd7af5cc64ea415f4c2b03bd9692e1b
Johan van Tent <tentvanjohan@hotmail.com>
2021-04-03 12:05:12 +0200

    naam gewijzigd

--- tests/testthat/test-dom_overzicht.R
+++ tests/testthat/test-dom_overzicht.R
@@ -4,7 +4,7 @@ test_that("dom_overzicht_basis", {
   # skip("Duurt relatief lang")
   skip_on_cran()
 
-  expect_gt(nrow(dom_overzicht_basis()), 250)
+  expect_gt(nrow(suppressWarnings(dom_overzicht_basis())), 250)
 })
 
 test_that("Caching dom_overzicht", {
