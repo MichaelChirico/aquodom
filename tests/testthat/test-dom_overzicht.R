c1dc3375bcf4cc163788cd6979eca72c1eaf768a
Johan van Tent <tentvanjohan@hotmail.com>
2021-04-03 12:08:26 +0200

    tests verplaatst

--- tests/testthat/test-dom_overzicht.R
+++ tests/testthat/test-dom_overzicht.R
@@ -14,12 +14,4 @@ test_that("Caching dom_overzicht", {
   expect_true(system.time(dom_overzicht())[3] < 0.5)
 })
 
-test_that("is_domeintabel",{
-  skip_on_cran()
-  expect_equal(is_domeintabel(c("Hoedanigheid", "Domeintabel")), c(TRUE, FALSE))
-})
 
-test_that("domeintabel_guid",{
-  skip_on_cran()
-  expect_equal(dom_guid(c("Hoedanigheid", "Domeintabel")), c("Id-7169dd0a-813b-4cf1-86ab-9bbc52b113a4", NA))
-})
