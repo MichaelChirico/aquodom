e70e7c747f96e7875efe77090b65099c67b17e65
Johan van Tent <tentvanjohan@hotmail.com>
2021-04-03 13:55:57 +0200

    tests voor dom_overzicht

--- tests/testthat/test-dom_overzicht.R
+++ tests/testthat/test-dom_overzicht.R
@@ -15,3 +15,18 @@ test_that("Caching dom_overzicht", {
 })
 
 
+test_that("dom_overzicht", {
+
+  overzicht <- dom_overzicht()
+
+  expect_gt(nrow(overzicht), 250)
+  expect_equal(nrow(dom_overzicht(peildatum = "2021-04-03")), 126)
+  expect_equal(nrow(dom_overzicht(peildatum = as.Date("2021-04-03"))), 126)
+
+  expect_s3_class(overzicht$wijzigingsdatum, "Date")
+  expect_s3_class(overzicht$begin_geldigheid, "Date")
+  expect_s3_class(overzicht$eind_geldigheid, "Date")
+  expect_type(overzicht$kolommen, "list")
+  expect_type(overzicht$kolommen[[1]], "character")
+
+})
