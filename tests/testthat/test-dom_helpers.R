cf4a4ed7c3f12e78b794a3e1d31cc3e1c1ea0bca
Johan van Tent <tentvanjohan@hotmail.com>
2021-04-03 13:59:32 +0200

    test voor dom_kolommen

--- tests/testthat/test-dom_helpers.R
+++ tests/testthat/test-dom_helpers.R
@@ -3,7 +3,12 @@ test_that("is_domeintabel",{
   expect_equal(is_domeintabel(c("Hoedanigheid", "Domeintabel")), c(TRUE, FALSE))
 })
 
-test_that("domeintabel_guid",{
+test_that("dom_guid",{
   skip_on_cran()
   expect_equal(dom_guid(c("Hoedanigheid", "Domeintabel")), c("Id-7169dd0a-813b-4cf1-86ab-9bbc52b113a4", NA))
+})
+
+test_that("dom_kolommen",{
+  skip_on_cran()
+  expect_equal(dom_kolommen("MonsterType"), c("Begin geldigheid", "Eind geldigheid", "Gerelateerd", "Id", "Omschrijving"))
 })
