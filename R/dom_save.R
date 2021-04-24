90ab05e93e406df01704f16e1447cf5cf9a98a0f
Johan van Tent <tentvanjohan@hotmail.com>
2021-04-24 15:38:46 +0200

    eerste versie dom_save voor het opslaan van domeintabellen

---
+++ R/dom_save.R
@@ -0,0 +1,26 @@
+dom_save <- function(naam, bestandsnaam, bestandstype = c("xlsx", "csv"), peildatum = Sys.Date()) {
+
+  bestandstype <- bestandstype[[1]]
+  rlang::arg_match(bestandstype, c("xlsx", "csv"))
+
+  dom_tabel <- dom(naam, peildatum)
+
+  if (bestandstype == "xlsx") {
+    bestandsnaam <- bestandsnaam %>% stringr::str_remove(".xlsx$") %>% stringr::str_c(".xlsx")
+    openxlsx::write.xlsx(dom_tabel, file = bestandsnaam)
+  }
+
+  if (bestandstype == "csv") {
+    bestandsnaam <- bestandsnaam %>% stringr::str_remove(".csv$") %>% stringr::str_c(".csv")
+    readr::write_csv2(dom_tabel, path = bestandsnaam)
+  }
+
+
+}
+
+# dom_save("MonsterType", "DEV/test.xlsx", bestandstype = "xlsx")
+# dom_save("MonsterType", "DEV/test.xlsx")
+#
+# dom_save("MonsterType", "DEV/test.csv", "csv", "20000101")
+
+
