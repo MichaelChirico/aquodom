594b8debbfd8c45c5548933a38aad1ff788d0ecf
Johan van Tent <tentvanjohan@hotmail.com>
2021-04-27 09:30:01 +0200

    default bestandsnaam toegevoegd

--- R/dom_save.R
+++ R/dom_save.R
@@ -1,4 +1,22 @@
-dom_save <- function(naam, bestandsnaam, bestandstype = c("xlsx", "csv"), peildatum = Sys.Date()) {
+#' Opslaan domeintabel
+#'
+#' Met deze functie is het mogelijk om een domeintabel op te slaan als .xlsx-bestand of als .csv-bestand.
+#'
+#' @param naam Naam van een domeintabel
+#' @param bestandsnaam
+#' @param map
+#' @param bestandstype
+#' @param peildatum
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dom_save <- function(naam,
+                     bestandsnaam = paste(Sys.Date(), naam),
+                     map = NULL,
+                     bestandstype = c("xlsx", "csv"),
+                     peildatum = Sys.Date()) {
 
   bestandstype <- bestandstype[[1]]
   rlang::arg_match(bestandstype, c("xlsx", "csv"))
@@ -7,11 +25,15 @@ dom_save <- function(naam, bestandsnaam, bestandstype = c("xlsx", "csv"), peilda
 
   if (bestandstype == "xlsx") {
     bestandsnaam <- bestandsnaam %>% stringr::str_remove(".xlsx$") %>% stringr::str_c(".xlsx")
+    if (!is.null(map)) bestandsnaam <- file.path(map, bestandsnaam)
+
     openxlsx::write.xlsx(dom_tabel, file = bestandsnaam)
   }
 
   if (bestandstype == "csv") {
     bestandsnaam <- bestandsnaam %>% stringr::str_remove(".csv$") %>% stringr::str_c(".csv")
+    if (!is.null(map)) bestandsnaam <- file.path(map, bestandsnaam)
+
     readr::write_csv2(dom_tabel, path = bestandsnaam)
   }
 
@@ -25,4 +47,11 @@ dom_save <- function(naam, bestandsnaam, bestandstype = c("xlsx", "csv"), peilda
 #
 # dom_save("MonsterType", "DEV/test.csv", "csv", "20000101")
 # file.remove("DEV/test.csv")
-
+# dom_save("MonsterType")
+# file.remove(paste(Sys.Date(),"MonsterType.xlsx"))
+# dom_save("MonsterType", bestandstype = "csv")
+# file.remove(paste(Sys.Date(),"MonsterType.csv"))
+# dom_save("MonsterType", map = "DEV")
+# file.remove(file.path("DEV", paste(Sys.Date(),"MonsterType.xlsx")))
+#
+# naam <- "MonsterType"
